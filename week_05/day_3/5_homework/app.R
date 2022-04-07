9f7229e51ef4753137d81aa804a6d24a3ab898b0
guild13 <63346560+guild13@users.noreply.github.com>
2022-04-07 08:30:20 +0100

    wk5day3 hw

--- week_05/day_3/5_homework/app.R
+++ week_05/day_3/5_homework/app.R
@@ -1,22 +1,47 @@
 library(shiny)
+library(here)
 library(tidyverse)
+library(ggplot2)
+library(shinythemes)
 
-amazon_reviews <- CodeClanData::amazon_reviews
+amazon_reviews <- read_csv(here("data/amazon_reviews.csv"))
 
 product_list <- unique(amazon_reviews$name)
 
 ui <- fluidPage(
+  
+  theme = shinytheme("yeti"),
+  
   mainPanel(
+    
     tabsetPanel(
-      tabPanel("Plot", plotOutput("plot")), 
-      tabPanel("Summary", verbatimTextOutput("summary")), 
-      tabPanel("Table", tableOutput("table"))
+      
+      id = "name_input",
+      
+      tabPanel(title = "Amazon Kindle", value = "Kindle", plotOutput("plot")), 
+      tabPanel(title = "Amazon Echo", value = "Echo", plotOutput("plot")), 
+      tabPanel(title = "Amazon Fire Tablet", value = "Fire Tablet", plotOutput("plot"))
+
     )
   )
 )
 
 server <- function(input, output, session) {
   
+  output$plot <- renderPlot({
+    
+    amazon_reviews %>%
+      filter(name == input$name_input) %>%
+      ggplot() +
+      aes(x = date, y = rating) +
+      geom_point() +
+      # labs(x = "\nteam",
+      #      y = "number of medals\n") +
+      # scale_fill_manual(values = c("Gold" = "#FFD700", "Silver" = "#C0C0C0", "Bronze" = "#CD7F32")) +
+      theme_minimal() +
+      theme(legend.position = "none")
+      
+  })
 }
 
 shinyApp(ui, server)
